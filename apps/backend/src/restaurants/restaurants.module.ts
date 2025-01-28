b65570b5f8276adee6945841fbcaed88d30f3bcb
hraby <75084936+Hraby@users.noreply.github.com>
2025-01-28 11:23:51 +0100

    fix: restaurant module

--- apps/backend/src/restaurants/restaurants.module.ts
+++ apps/backend/src/restaurants/restaurants.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { RestaurantsController } from './restaurants.controller';
 import { RestaurantsService } from './restaurants.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [RestaurantsController],
   providers: [RestaurantsService],
-  imports: [PrismaModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+  ],
 })
 export class RestaurantsModule {}
