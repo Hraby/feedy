9ddb9ab991e92f4b7355dabac0f6e4cad02cd993
Honza Běhunčík <112960672+honzabehuncik@users.noreply.github.com>
2025-02-04 16:17:24 +0100

    feat: basics for landing page

---
+++ apps/frontend/web/src/app/components/LandingNavbar.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="py-4">
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        <div className="text-[var(--primary)] font-bold text-3xl">feedy.</div>
+
+        <div className="flex gap-4">
+          <button className="bg-gradient-to-r from-[var(--gradient-start)] to-[var(--gradient-end)] text-white px-4 py-2 rounded-full font-medium transition-all duration-300 hover:brightness-110 hover:scale-105">
+            Přihlásit se
+          </button>
+          <button className="bg-[var(--gray)] px-4 py-2 rounded-full text-[var(--font)] font-medium transition-all duration-300 hover:bg-gray-300 hover:scale-105">
+            Registrace
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
