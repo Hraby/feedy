903dd6b063b901fc038ee3dd32bb214d39913be5
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2025-02-20 09:22:47 +0100

    feat: delivery page

--- apps/frontend/mobile/app/(tabs)/_layout.tsx
+++ apps/frontend/mobile/app/(tabs)/_layout.tsx
@@ -76,6 +76,15 @@ export default function TabLayout() {
           ),
         }}
       /> 
+       <Tabs.Screen
+        name="delivery"
+        options={{
+          title: 'Delivery',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      /> 
     </Tabs>
   );
 }
