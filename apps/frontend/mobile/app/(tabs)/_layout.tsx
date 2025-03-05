e3a8e54735d2d3fcbdf59c44ccd0375cafc03c1f
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2025-03-05 20:59:59 +0100

    feat: courier taking orders page

--- apps/frontend/mobile/app/(tabs)/_layout.tsx
+++ apps/frontend/mobile/app/(tabs)/_layout.tsx
@@ -130,6 +130,15 @@ export default function TabLayout() {
           ),
         }}
       /> 
+      <Tabs.Screen
+        name="takeorders"
+        options={{
+          title: 'Take',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      /> 
     </Tabs>
   );
 }
