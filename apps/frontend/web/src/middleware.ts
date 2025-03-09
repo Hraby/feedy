3ff0236d378b42e6715e47b2b34636d2017fe950
hraby <75084936+Hraby@users.noreply.github.com>
2025-03-09 22:26:21 +0100

    feat(web): middleware admin and profile

--- apps/frontend/web/src/middleware.ts
+++ apps/frontend/web/src/middleware.ts
@@ -3,12 +3,20 @@ import { getSession } from "./lib/session";
 
 export default async function middleware(req: NextRequest) {
   const session = await getSession();
-  if (!session || !session.user)
-    return NextResponse.redirect(new URL("/auth/signin", req.nextUrl));
+
+  if(req.nextUrl.pathname.startsWith("/admin")){
+    if (!session)
+      return NextResponse.redirect(new URL("/login", req.nextUrl));
+  
+    if (typeof session.user.role !== "string" || session.user.role !== "Admin") {
+      return NextResponse.redirect(new URL("/", req.nextUrl));
+    }
+  }
+
+  if(req.nextUrl.pathname.startsWith("/profile")){
+    if (!session)
+      return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
 
   NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/dashboard/:path"],
-};
