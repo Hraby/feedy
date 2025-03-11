9a54ae500c1f339ccd7feac108d13ec27abbc607
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2025-03-11 10:40:50 +0100

    Update usermenu.tsx

--- apps/frontend/mobile/app/(tabs)/usermenu.tsx
+++ apps/frontend/mobile/app/(tabs)/usermenu.tsx
@@ -157,7 +157,7 @@ const styles = StyleSheet.create({
   toggleText: {
     marginLeft: 10,
     fontSize: 16,
-    fontWeight: 'light'
+    fontWeight: 'light',
   },
   supplierIcon: {
     width: 45,
