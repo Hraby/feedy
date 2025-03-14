a7d4e8fd2be2863bd6999f0ffb9572393c512b23
honzabehuncik <honzabehuncik@gmail.com>
2025-03-15 00:49:47 +0100

    feat: landing page framer motion animations

--- apps/frontend/web/src/app/page.tsx
+++ apps/frontend/web/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import AppWidget from "../components/LandingWidget";
 import Footer from "../components/Footer";
@@ -5,32 +7,90 @@ import { FiMapPin } from 'react-icons/fi';
 import { FaShoppingBasket, FaBolt, FaStar } from 'react-icons/fa';
 import JoinFeedy from "../components/JoinFeedy";
 import NavbarSwitcher from "@/components/NavbarSwitch";
+import { motion, AnimatePresence } from 'framer-motion';
+import { useState, useEffect } from 'react';
+import { useAuth } from "@/contexts/AuthProvider";
 
-export default async function Home() {
+const slogans = [
+  "Doručíme vám jídlo rychlostí blesku!",
+  "Objevte nové restaurace v okolí!",
+  "Podporujte s námi místní podniky!",
+  "Vaše oblíbené jídlo je na dosah ruky!",
+  "Měníme způsob objednávání jídla!",
+  "Rychle a jednoduše - to je feedy!"
+];
+
+const fadeInUp = {
+  hidden: { opacity: 0, y: 30 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.6 } }
+};
+
+const fadeIn = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 0.6 } }
+};
+
+export default function Home() {
+  const [sloganIndex, setSloganIndex] = useState(0);
+  const { user, accessToken } = useAuth();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSloganIndex((prevIndex) => (prevIndex + 1) % slogans.length);
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
 
   return (
     <>
       <div className="bg-white">
-      <NavbarSwitcher />
+        <NavbarSwitcher />
         <div className="container mx-auto px-4">
-          <section className="min-h-[85vh] flex flex-col lg:flex-row items-center justify-between py-30 lg:py-16 text-center lg:text-left pt-20">
+        <section className="min-h-[85vh] flex flex-col lg:flex-row items-center gap-4 lg:gap-64 py-30 lg:py-16 text-center lg:text-left pt-20">
             <div className="max-w-3xl">
-              <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold leading-snug bg-[var(--primary)] bg-clip-text text-transparent">
-                Objevte nové restaurace ve vašem okolí!
-              </h1>
-              <div className="mt-4 relative w-full max-w-md mx-auto lg:mx-0 lg:text-left flex justify-center lg:justify-start">
-                <FiMapPin className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" size={20} />
-                <input
-                  type="text"
-                  placeholder="Vložte doručovací adresu"
-                  className="bg-[var(--gray)] px-10 py-2 rounded-full w-full pl-10 text-left lg:text-left"
+              <AnimatePresence mode="wait">
+                <motion.h1
+                  key={sloganIndex}
+                  initial={{ opacity: 0, y: 21 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -21 }}
+                  transition={{ duration: 0.6 }}
+                  className="text-4xl sm:text-5xl lg:text-6xl font-bold p-1 bg-gradient-to-r from-[var(--gradient-start)] to-[var(--gradient-end)] bg-clip-text text-transparent"
+                >
+                  {slogans[sloganIndex]}
+                </motion.h1>
+              </AnimatePresence>
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                variants={fadeInUp}
+              >
+                <div className="mt-4 relative w-full max-w-md mx-auto lg:mx-0 lg:text-left flex justify-center lg:justify-start">
+                  <button className="bg-gradient-to-r from-[var(--gradient-start)] to-[var(--gradient-end)] transition-transform duration-300 hover:scale-105 text-white px-10 py-2 rounded-full text-lg font-semibold shadow-lg" onClick={() => window.location.href = user ? '/menu' : '/login'}>
+                    {user ? 'Mám hlad!' : 'Nejprve se přihlásím!'}
+                  </button>
+                </div>
+              </motion.div>
+            </div>
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              variants={fadeInUp}
+            >
+              <div className="mt-8 lg:mt-0">
+                <Image
+                  src="/img/burger.png"
+                  alt="Burger a hranolky"
+                  width={450}
+                  height={350}
+                  className="mx-auto animate-pulse-burger-pause"
                 />
               </div>
-
-            </div>
-            <div className="mt-8 lg:mt-0">
-              <Image src="/img/burger.png" alt="Burger a hranolky" width={450} height={350} className="mx-auto" />
-            </div>
+            </motion.div>
           </section>
         </div>
       </div>
@@ -57,28 +117,48 @@ export default async function Home() {
               { title: "Doručení rychlostí blesku", text: "Zapomeňte na nekonečné čekání a objednejte si rychlostí blesku.", icon: <FaBolt /> },
               { title: "Získejte odměny za objednávky", text: "Čím více objednáte, tím více získáte odměn a slev na další objednávky.", icon: <FaStar /> },
             ].map((item, index) => (
-              <div
+              <motion.div
                 key={index}
-                className="bg-white p-8 lg:p-10 lg:py-12 rounded-card shadow-md text-center max-w-sm mx-auto min-h-[200px] flex flex-col justify-between"
+                className="bg-white p-8 lg:p-10 lg:py-20 rounded-card shadow-md text-center max-w-sm mx-auto min-h-[200px] flex flex-col justify-between"
+                initial={{ opacity: 0, y: 30 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.6, delay: index * 0.2 }}
               >
                 <div className="bg-[var(--primary)] p-4 rounded-full inline-flex items-center justify-center w-16 h-16 mx-auto text-white text-3xl">
                   {item.icon}
                 </div>
                 <h3 className="text-xl lg:text-2xl font-bold text-[var(--primary)] mt-5">{item.title}</h3>
                 <p className="text-[var(--font)] mt-2">{item.text}</p>
-              </div>
+              </motion.div>
             ))}
           </div>
         </div>
       </section>
 
       <section className="bg-[#EFEFEF] pb-12 lg:pb-16 pt-24">
-        <div className="container mx-auto px-4">
-          <AppWidget />
-        </div>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={fadeInUp}
+        >
+          <div className="container mx-auto px-4">
+            <AppWidget />
+          </div>
+        </motion.div>
       </section>
 
-      <JoinFeedy></JoinFeedy>
+      <section className="bg-[#EFEFEF]">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={fadeInUp}
+        >
+          <JoinFeedy></JoinFeedy>
+        </motion.div>
+      </section>
 
       <Footer />
     </>
