d1af6999a5ec9cb620219580119014b0c309e4c9
hraby <75084936+Hraby@users.noreply.github.com>
2026-03-19 09:58:53 +0100

    fix: eslint

--- apps/frontend/web/next.config.ts
+++ apps/frontend/web/next.config.ts
@@ -7,10 +7,7 @@ const nextConfig: NextConfig = {
     MAPY_API_KEY: process.env.MAPY_API_KEY,
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
